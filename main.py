fa56254f82e2ea1ba8807c8e674cac6135b63790
PineApple-Logic <63014709+PineApple-Logic@users.noreply.github.com>
2021-11-15 13:00:22 +0200

    Update

--- main.py
+++ main.py
@@ -1,45 +1,49 @@
 from pandas import DataFrame
 import os
-from datetime import date
 from os.path import exists
+from datetime import date
 
 # Variables
-path = "./slips"
 today = date.today()
+path = "./slips"
 x = {"Date": [],
      "Category": [],
      "Price": [],
      "Details": []
      }
-date = f'{today.strftime("%d-%b-%Y")}.csv'
+date = f'./{today.strftime("%d-%b-%Y")}.csv'
 
 # Folder
 if not exists('./slips'):
     os.mkdir('./slips')
-print('Place the receipt in the Resets Folder. It was created')
-print('(Click anything to continue)')
-input()
+print('Place the receipt in the "slips" Folder. It was created')
+print()
+input('Press Enter to continue')
+print()
 
 # Capturing slips
 filenames = os.listdir(path)
-print(filenames)
 for filename in filenames:
     names = filename[:-4].split()
-    print(names)
-    input()
-    x['Date'].append(names[0])
-    x['Category'].append(names[1])
-    price = names[2]
-    if price[-1] == ')':
-        x['Price'].append(price[:-3])
-        x['Details'].append(price[-3:])
+    if len(names) >= 4 or len(names) <= 2:
+        print(f'Note that there is an abnormal amount of words/number sets for slip  ({filename})')
+        print('It will not be recorded')
+        print()
+        input('Press Enter to continue')
+        print()
     else:
-        x['Price'].append(price)
+        x['Date'].append(names[0])
+        x['Category'].append(names[1])
+        price = names[2]
+        if price[-1] == ')':
+            x['Price'].append(price[:-3])
+            x['Details'].append(price[-3:])
+        else:
+            x['Price'].append(price)
 
 # Load slips to a CSV file
 if x['Details'] != '':
     df = DataFrame(x, columns=["Date", "Category", "Price", "Details"])
 else:
     df = DataFrame(x, columns=["Date", "Category", "Price"])
-print(df)
 df.to_csv(date)
