8ac520eb6bce0813f66e474875e064bad7642d93
PineApple-Logic <63014709+PineApple-Logic@users.noreply.github.com>
2021-11-19 11:08:24 +0200

    Update
    Bug fix: name length not matching.
    
    Added feature: Changes O, A, C, E, T, F, S to there full name when recorded

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
 from pandas import DataFrame
-import pandas as pd
 import os
 from os.path import exists
 from datetime import date
@@ -14,6 +13,14 @@ x = {"Date": [],
      "Duplicate": []
      }
 date = f'./{today.strftime("%d-%b-%Y")}.csv'
+
+
+def match_len(num):
+    if len(names[num]) != 12:
+        add_to = 12 - len(names[num])
+        if len(names[num]) != 12:
+            names[num].ljust(len(names[num]) + add_to)
+
 
 # Folder
 if not exists('./slips'):
@@ -36,7 +43,37 @@ for filename in filenames:
         print()
     else:
         x['Date'].append(names[0])
+        match_len(1)
         x['Details'].append(names[1])
+        if names[2] == 'O':
+            match_len(2)
+            x['Category'].append('Other')
+        elif names[2] == 'A':
+            names[2] = 'Accommodation'
+            match_len(2)
+        elif names[2] == 'C':
+            names[2] = 'Consumables'
+            match_len(2)
+        elif names[2] == 'E':
+            names[2] = 'Equipment'
+            match_len(2)
+        elif names[2] == 'T':
+            names[2] = 'Toll'
+            match_len(2)
+        elif names[2] == 'F':
+            names[2] = 'Food'
+            match_len(2)
+        elif names[2] == 'S':
+            names[2] = 'Stationery'
+            match_len(2)
+        elif len(names[2]) >= 2:
+            match_len(2)
+        else:
+            print(f"Failed to determine the Category's Full name of {names[2]}")
+            print()
+            full_name = input('Full name:')
+            names[2] = full_name
+            match_len(2)
         x['Category'].append(names[2])
         x['Price'].append(names[3])
         if len(names) == 5:
@@ -51,4 +88,3 @@ if x['Details'] != '':
 else:
     df = DataFrame(x, columns=["Date", "Details", "Category", "Price"])
 df.to_csv(date)
-
